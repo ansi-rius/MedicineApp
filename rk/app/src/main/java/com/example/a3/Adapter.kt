c6c1cab48f80c8368d2191ac79200eecafaa8f3c
PsycholoG_ZxZ <dimka_volnyi@mail.ru>
2020-10-25 20:46:45 +0300

    add basic navigation and intent, change linear to relative in main xml, intent - inf reboot mainActivity for tests

--- rk/app/src/main/java/com/example/a3/Adapter.kt
+++ rk/app/src/main/java/com/example/a3/Adapter.kt
@@ -1,5 +1,6 @@
-package com.example.a3
+package com.example
 
+import android.content.Intent
 import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
@@ -9,10 +10,18 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.list_item_view.view.*
 import android.content.res.Resources
+import android.widget.Button
 import androidx.appcompat.content.res.AppCompatResources.getDrawable
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.example.a3.MainActivity
+import com.example.a3.Medecine
+import com.example.a3.R
 
 class Adapter(val items: List<Medecine>, private val clickListener: (Medecine) -> Unit) :
     RecyclerView.Adapter<Adapter.ViewHolder>() {
+
+
 
     override fun getItemCount(): Int {
         return items.size
@@ -28,24 +37,44 @@ class Adapter(val items: List<Medecine>, private val clickListener: (Medecine) -
         viewHolder.bind(items[position], clickListener)
     }
 
+
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         var largeTextView: TextView? = null
         var smallTextView: TextView? = null
         var picImageView: ImageView? = null
+        //lateinit var NavControler: NavController
+
+
+
         init {
             largeTextView = itemView?.findViewById(R.id.textViewLarge)
             smallTextView = itemView?.findViewById(R.id.textViewSmall)
             picImageView = itemView?.findViewById(R.id.imageView)
+            //NavControler = Navigation.findNavController(itemView)
         }
 
         fun bind(med: Medecine, clickListener: (Medecine) -> Unit) {
+
             itemView.textViewLarge.text = med.medecineName[med.id]
             itemView.textViewSmall.text = "more info"
 
             itemView.imageView.setImageResource(med.pics[med.id])
 
             itemView.setOnClickListener { clickListener(med) }
-            itemView.setOnClickListener{ itemView.setBackgroundColor(Color.parseColor("#C8C8C8")) }
+            itemView.setOnClickListener{
+                itemView.setBackgroundColor(Color.parseColor("#C8C8C8"))
+                //Navigation
+                val intent = Intent(itemView.context, MainActivity::class.java)
+                itemView.context.startActivity(intent)
+
+                //NavControler.navigate(R.id.action_homeFragment_to_description) //action_homeFragment_to_description
+
+
+
+
+            }
         }
     }
+
+
 }
--- rk/app/src/main/java/com/example/a3/MainActivity.kt
+++ rk/app/src/main/java/com/example/a3/MainActivity.kt
@@ -3,14 +3,22 @@ package com.example.a3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.Adapter
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var NavControler: NavController
+
     var listOfMedecines: ArrayList<Medecine> = ArrayList()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView (R.layout.activity_main)
+
 
         for (i in 0..15) {
             val med = Medecine()
@@ -20,6 +28,8 @@ class MainActivity : AppCompatActivity() {
         my_recycler_view.layoutManager = LinearLayoutManager(this)
         my_recycler_view.setHasFixedSize(true)
         my_recycler_view.adapter = Adapter(listOfMedecines, { med -> medItemClicked(med) })
+
+        NavControler = findNavController(R.id.nav_fragment)
     }
     private fun medItemClicked(med: Medecine) {
         Toast.makeText(this, "Clicked: ${med.medecineName[med.id]}", Toast.LENGTH_SHORT).show()
